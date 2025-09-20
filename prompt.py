0bcca85ab32bca8ba8441365dc37b19169bc26ac
kasrarh <55912648+kasrarh@users.noreply.github.com>
2025-09-20 15:38:05 -0400

    Adding neo4j functions as comments

--- prompt.py
+++ prompt.py
@@ -1,13 +1,29 @@
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+# from neo4j import GraphDatabase
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
-
 client = OpenAI(api_key=api_key)
 
-def ask_gpt(prompt, system_role="You have to asnwer in Cypher query format. Nothing else."):
+#--------- Graph Database -------------------------
+# driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+
+# def get_schema():
+#     schema = []
+#     with driver.session() as session:
+#         result = session.run("CALL db.schema.visualization()")
+#         for record in result:
+#             schema.append(record.data())
+#     return schema
+
+# schema_info = str(get_schema())
+
+
+#def ask_gpt(prompt, system_role=f"You have to asnwer in Cypher query format. Nothing else. Schema: {schema_info}"):
+#--------------------------------------------------
+def ask_gpt(prompt, system_role=f"You have to asnwer in Cypher query format. Nothing else."):
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
